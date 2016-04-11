66548808397832ee98a2393880961febd9cc8612
braudeproj2016 <braudeproj2016@gmail.com>
2016-04-11 12:17:00 +0300

    Revert "Update Program.cs"
    This reverts commit 3387d7007eb3c04366b0c53109da84a536f5cbec.

--- v1/v1/Program.cs
+++ v1/v1/Program.cs
@@ -11,7 +11,6 @@ namespace v1
         static void Main(string[] args)
         {
             System.Console.WriteLine("Heelo World!");
-            System.Console.Write("Hi");
         }
     }
 }
