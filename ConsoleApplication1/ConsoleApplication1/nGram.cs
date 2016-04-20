b14862a5403c347a45751f02b3dd07a40ad0caf7
braudeproj2016 <braudeproj2016@gmail.com>
2016-04-20 13:32:48 +0300

    adding our code

---
+++ ConsoleApplication1/ConsoleApplication1/nGram.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class nGram
+    {
+        protected readonly string s;
+        protected int c;
+        protected nGram n;
+        static int amunt = 0;
+        public nGram(string s)
+        {
+            this.s = s;
+            c = 1;
+            n = null;
+            amunt++;
+        }
+        public void inc()
+        {
+            c++;
+        }
+        public string text
+        {
+            get
+            {
+                return s;
+            }
+        }
+        public int count
+        {
+            get
+            {
+                return c;
+            }
+        }
+        public nGram next
+        {
+            get
+            {
+                return n;
+            }
+            set
+            {
+                n = value;
+            }
+        }
+        public nGram operator =(nGram ng)
+        {
+            this.c = ng.c;
+            this.n = ng.n;
+            this.t = ng.s;
+        }
+    }
+}
--- v1/v1/Program.cs
+++ v1/v1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,75 @@ namespace v1
         {
             System.Console.WriteLine("Heelo World!");
             System.Console.Write("Hi");
+            runit();
+        }
+
+        public static void preparefile()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            StreamReader sr = new StreamReader("stopwords.txt");
+            List<string> listOfStopWords = new List<string>();
+            while (!sr.EndOfStream)
+                listOfStopWords.Add(sr.ReadLine());
+            sr.Dispose();
+            StreamReader sr2 = File.OpenText("tmp.txt");
+            StreamWriter sw = File.CreateText("out2.txt");
+            string s;
+            while (!sr2.EndOfStream)
+            {
+                s = Regex.Replace(sr2.ReadLine().ToLower(), @"[^\w\s]", " ");
+                foreach (string word in listOfStopWords)
+                {
+                    s = s.Replace(" " + word + " ", " ");
+                }
+                sw.WriteLine(Regex.Replace(s, @"\s+", " "));
+            }
+            sr2.Dispose();
+            sw.Dispose();
+        }
+
+        public static int hashValue(string st, int i)
+        {
+            return (Convert.ToInt32(st[i]) * 3 + Convert.ToInt32(st[i + 1]) * 10 + Convert.ToInt32(st[i + 2]) * 4) % 4567;
+        }
+
+        public static void update(out nGram a, ref nGram b)
+        {
+            a = b;
+        }
+
+        static void runit()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            //  Program.preparefile();
+            StreamReader sr = File.OpenText("out2.txt");
+            string text = "";
+            while (!sr.EndOfStream)
+                text = text + " " + sr.ReadLine();
+            sr.Dispose();
+
+            //WriteLine(text);
+            nGram[] ht = new nGram[4567];
+            //  nGram tmpNG = null;
+            Console.WriteLine(ht[1]);
+            string tmpSt;
+            int tmp;
+            for (int i = 0; i < text.Length - 2; i++)
+            {
+                tmpSt = "" + text[i] + text[i + 1] + text[i + 2];
+                tmp = hashValue(tmpSt, 0);
+                nGram tmpNG;//= ht[tmp];
+                //tmpNG = &ht[tmp];
+                update(out tmpNG, ref ht[tmp]); //<<===our problem
+
+                while (tmpNG != null && !(tmpNG.text.Equals(tmpSt)))
+                    tmpNG = tmpNG.next;
+                if (tmpNG == null)
+                    tmpNG = new nGram(tmpSt);
+                else
+                    tmpNG.inc();
+            }
+            Console.Read();
         }
     }
 }
